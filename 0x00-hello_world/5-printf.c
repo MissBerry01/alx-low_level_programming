778f058fa27dc72a1a4f09af4484edd4aba170f6
MissBerry01 <mwongelamorine9@gmail.com>
2023-08-15 23:57:22 -0700

    a program that prints all possible different combinations of three digits.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a C program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line
+ * main - prints all possible different combinations of three digits.
  *
  * Return: 0 (Success)
  */
