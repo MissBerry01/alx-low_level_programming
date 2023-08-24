09f88dbe68f36ab7e172d2ae3e87117c79d2d6f2
MissBerry01 <mwongelamorine9@gmail.com>
2023-08-23 23:21:28 -0700

    a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,36 +1,26 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes a character to the standard output
- * @c: The character to print
- * Return: On success 1. On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
 
 /**
  *  print_number -  a function that prints an integer.
- * @n: integer
+ * @n: integer params
  * Return: 0
  */
+
 void print_number(int n)
 {
 
-unsigned int x1;
+unsigned int n1;
 
-x1 = (n < 0) ? -n : n;
+n1 = n;
 if ( n< 0)
 {
-_putchar('-');
-}
 
-if (x1 / 10 != 0)
-{
-print_number(x1 / 10);
+_putchar('-');
+n1 = -n;
 }
-_putchar((x1 % 10) + '0');
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
