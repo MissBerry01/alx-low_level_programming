195fd3fb8fac8122e58007e33d772b8ab07cced3
MissBerry01 <mwongelamorine9@gmail.com>
2023-09-07 02:27:10 -0700

    a function that reallocates a memory block using malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: void pointer
+ * @old_size: initial size
+ * @new_size: new size
+ * Return: pointer to new size
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *n;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size * sizeof(void *));
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	n = malloc(new_size * sizeof(char));
+	if (n == NULL)
+		return (NULL);
+	i = 0;
+	if (new_size > old_size)
+	{
+		while (i < old_size)
+		{
+			n[i] = ((char *)ptr)[i];
+			i++;
+		}
+		free(ptr);
+		return (n);
+	}
+	while (i < new_size)
+	{
+		n[i] = ((char *)ptr)[i];
+		i++;
+	}
+	free(ptr);
+	return (n);
+}
