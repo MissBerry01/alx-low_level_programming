a078deba7ebdebc7ecf79cb65029e7b0a32ac9c8
MissBerry01 <mwongelamorine9@gmail.com>
2023-09-21 02:30:27 -0700

    a function that prints all the elements of a list_t list.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 #define ERR_MSG "Error"
 
 int find_len(char *str);
-char *create_xarray(int size);
+int is_digit(char *str);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
@@ -27,33 +26,6 @@ int find_len(char *str)
 }
 
 /**
- * create_xarray - Creates an array of chars and initializes it with
- *                 the character 'x'. Adds a terminating null byte.
- * @size: The size of the array to be initialized.
- *
- * Description: If there is insufficient space, the
- *              function exits with a status of 98.
- * Return: A pointer to the array.
- */
-char *create_xarray(int size)
-{
-	char *array;
-	int index;
-
-	array = malloc(sizeof(char) * size);
-
-	if (array == NULL)
-		exit(98);
-
-	for (index = 0; index < (size - 1); index++)
-		array[index] = 'x';
-
-	array[index] = '\0';
-
-	return (array);
-}
-
-/**
  * iterate_zeroes - Iterates through a string of numbers containing
  *                  leading zeroes until it hits a non-zero number.
  * @str: The string of numbers to be iterate through.
@@ -66,30 +38,28 @@ char *iterate_zeroes(char *str)
 		str++;
 
 	return (str);
-}
 
 /**
- * get_digit - Converts a digit character to a corresponding int.
- * @c: The character to be converted.
- *
- * Description: If c is a non-digit, the function
- *              exits with a status of 98.
- * Return: The converted int.
+ * is_digit - Checks if a string has digits
+ * @str: The string to check
+ * Return: 1 if it has digits and 0 otherwise
  */
-int get_digit(char c)
-{
-	int digit = c - '0';
 
-	if (digit < 0 || digit > 9)
+int is_digit(char *str)
+{
+	while (*str)
 	{
-		printf("Error\n");
-		exit(98);
+		if (*str > '9' || *str < '0')
+		{
+			return (0);
+		}
+		str++;
 	}
 
-	return (digit);
+	return (1);
 }
 
-/**
+/**	
  * get_prod - Multiplies a string of numbers by a single digit.
  * @prod: The buffer to store the result.
  * @mult: The string of numbers.
@@ -136,48 +106,6 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 
 	if (tens)
 		*prod = (tens % 10) + '0';
-}
-
-/**
- * add_nums - Adds the numbers stored in two strings.
- * @final_prod: The buffer storing the running final product.
- * @next_prod: The next product to be added.
- * @next_len: The length of next_prod.
- */
-void add_nums(char *final_prod, char *next_prod, int next_len)
-{
-	int num, tens = 0;
-
-	while (*(final_prod + 1))
-		final_prod++;
-
-	while (*(next_prod + 1))
-		next_prod++;
-
-	for (; *final_prod != 'x'; final_prod--)
-	{
-		num = (*final_prod - '0') + (*next_prod - '0');
-		num += tens;
-		*final_prod = (num % 10) + '0';
-		tens = num / 10;
-
-		next_prod--;
-		next_len--;
-	}
-
-	for (; next_len >= 0 && *next_prod != 'x'; next_len--)
-	{
-		num = (*next_prod - '0');
-		num += tens;
-		*final_prod = (num % 10) + '0';
-		tens = num / 10;
-
-		final_prod--;
-		next_prod--;
-	}
-
-	if (tens)
-		*final_prod = (tens % 10) + '0';
 }
 
 /**
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-C hello world
+0x12-singly_linked_listsC Programming
