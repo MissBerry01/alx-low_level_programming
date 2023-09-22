5819180631204f1d492a92fa4a11aaeb81fbcb28
MissBerry01 <mwongelamorine9@gmail.com>
2023-09-22 02:39:58 -0700

    Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,14 @@
-; My comments: I can write assembly code in any language
-; File: 101-hello_holberton.asm
-; Desc: 64-bit assembly program that prints
-;       Hello, Holberton followed by a new line.
-; Auth: Julien
-
 section .data
-	msg db "Hello, Holberton", 10, 0
-	fmt db "%n", 0
+	fmt db 'Hello, Holberton', 10, 0
 
 section .text
-    global main
-
+	global main
 	extern printf
 
 main:
-	push rbp
-	mov rdi, msg
-	mov rax, 0
+	mov rdi, format
+	xor rax, rax
 	call printf
-	pop rbp
-
-	; Exit the program
 	mov rax, 60
 	xor rdi, rdi
 	syscall
