a8e00b143bf52be60baf57cf7adf7f69d87f47c8
MissBerry01 <mwongelamorine9@gmail.com>
2023-09-22 01:39:49 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,3 +1,9 @@
+; My comments: I can write assembly code in any language
+; File: 101-hello_holberton.asm
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+; Auth: Julien
+
 section .data
 	msg db "Hello, Holberton", 10, 0
 	fmt db "%s", 0
@@ -18,8 +24,3 @@ main:
 	mov rax, 60
 	xor rdi, rdi
 	syscall
-
-; My comments: I can write assembly code in any language
-; File: 101-hello_holberton.asm
-; Desc: 64-bit assembly program that prints
-;       Hello, Holberton followed by a new line.
